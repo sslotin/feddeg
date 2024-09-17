5d3454b2d56a3a5a90e5b33baadb1b05f0768122
Sergey Slotin <me@sereja.me>
2024-09-17 12:28:50 +0300

    add filename argument to har hasher

--- har_hashes.py
+++ har_hashes.py
@@ -1,8 +1,15 @@
 import json
 import hashlib
 
+import argparse
 
-with open('testedg.deg.rt.ru.har', 'r') as file:
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument('file', type=str)
+args = parser.parse_args()
+
+with open(args.file, 'r') as file:
     data = json.loads(file.read())
     for entry in data['log']['entries']:
         if entry['_resourceType'] in ['document', 'script'] and 'text' in entry['response']['content']:
